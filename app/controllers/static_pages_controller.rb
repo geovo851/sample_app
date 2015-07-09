0605f3221a0a65c05802226b348e0c278dcfe81f
geovo851 <geovo851@gmail.com>
2015-07-09 07:28:22 -0700

    Add user microposts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,10 @@
 class StaticPagesController < ApplicationController
 
   def home
-    if current_user 
-      redirect_to current_user
-	end
+    if signed_in?
+      @micropost  = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
   end
 
   def help
